7dad525c6d087b9594cc8f4cb2cf57462ceae34c
Luiz Paullo <luizpc.contato@gmail.com>
2025-09-09 15:54:32 -0300

    Primeiro commit: backend da barbearia completo

--- projectbarber/src/main/java/com/projectbarber/projectbarber/controller/ServicoController.java
+++ projectbarber/src/main/java/com/projectbarber/projectbarber/controller/ServicoController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,7 +25,7 @@ public class ServicoController {
     }
 
     //Criar servico
-    @PostMApping
+    @PostMapping
     public ResponseEntity<Servico> criarServico(@RequestBody Servico servico) {
         return ResponseEntity.ok(servicoService.cadastrarServico(servico));
     }   
